c26a09fc930f50974b7f8b3dfbe89b9f35473f69
Senthuran100 <senthuran.manoharan@gmail.com>
2022-06-25 17:28:01 +0800

    Add Service implementation for the controllers.

--- src/main/java/com/demo/stream/Orders/service/OrderService.java
+++ src/main/java/com/demo/stream/Orders/service/OrderService.java
@@ -1,12 +1,19 @@
 package com.demo.stream.Orders.service;
 
+import com.demo.stream.Orders.exception.OrderNotFoundException;
 import com.demo.stream.Orders.model.Order;
 import com.demo.stream.Orders.model.OrderBinder;
 import com.demo.stream.Orders.model.OrderStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MimeTypeUtils;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 @Slf4j
@@ -14,9 +21,27 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderBinder orderBinder;
-    public  Order placeOrder(Order order) {
+
+    Map<UUID, Order> orderStore = new HashMap<>();
+
+    public Order placeOrder(Order orderReq) {
+        var order = Order.builder()
+                .orderStatus(OrderStatus.PENDING)
+                .orderUUID(orderReq.getOrderUUID())
+                .itemName(orderReq.getItemName())
+                .build();
+
+        orderStore.put(order.getOrderUUID(), order);
+        orderBinder.inventoryCheckingOut()
+                .send(MessageBuilder.withPayload(order)
+                        .setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON)
+                        .build());
+        return order;
+
     }
 
     public OrderStatus statusCheck(UUID uuid) {
+        return Optional.ofNullable(orderStore.get(uuid))
+                .orElseThrow(() -> new OrderNotFoundException("Order Not Found")).getOrderStatus();
     }
 }
